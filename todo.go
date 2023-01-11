5e5fd816749919f470afec3d1511f5b06fae4e50
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-11 20:05:25 +0800

    feat: add 5 essential methods for the List struct
    added the following methods:
    	- Add(): Creates a nw to-do item and appends it to the List
    	- Complete(): Marks a to-do item as completed
    	- Delete(): Deletes a to-do item from the List
    	- Save(): Saves the list of items to a file using the JSON
    	  format
    	- Get(): Obtains a list of items from a saved JSON file

--- todo.go
+++ todo.go
@@ -1,6 +1,12 @@
 package basic_todo_app
 
-import "time"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+)
 
 // item struct represents a ToDo item
 type item struct {
@@ -12,3 +18,71 @@ type item struct {
 
 // List represents a list of ToDo items
 type List []item
+
+// Add creates a nwe todo item and appends it to the list
+func (l *List) Add(task string) {
+	t := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Now(),
+	}
+	*l = append(*l, t)
+}
+
+// Complete marks a Todo item as completed by
+// setting Done = true and CompletedAt to the current time
+func (l *List) Complete(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("item %d does not exist", i)
+	}
+
+	// Adjusting index for 0 based index
+	ls[i-1].Done = true
+	ls[i-1].CompletedAt = time.Now()
+	return nil
+}
+
+// Delete deletes a ToDo item from the list
+func (l *List) Delete(i int) error {
+	ls := *l
+	if i <= 0 || i > len(ls) {
+		return fmt.Errorf("item %d does not exist", i)
+	}
+
+	// Adjusting index for 0 based index
+	*l = append(ls[:i-1], ls[i:]...)
+	return nil
+}
+
+// Save encodes the List as JSON and saves it
+// using the provided file name
+func (l *List) Save(filename string) error {
+	js, err := json.Marshal(l)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(filename, js, 0644)
+}
+
+// Get opens the provided file name, decodes
+// the JSON data and parses it into a List
+func (l *List) Get(filename string) error {
+	file, err := os.ReadFile(filename)
+	// we ignore the error if the file doesn't exist(or is empty (next block))
+	// presumably because if this method returns nil the consumer of this method
+	// will create the file or handle an empty output differently
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+
+	if len(file) == 0 {
+		return nil
+	}
+
+	return json.Unmarshal(file, l)
+}
